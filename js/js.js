f3a4b2bd302224bd669b8823890c4d5e00b75777
JonathanBergfastNTIJohanneberg <jonathan.bergfast@elev.ga.ntig.se>
2022-12-05 14:50:48 +0100

    Lektion 3, har gjort en del animationer men har fortfarande en del att göra

--- js/js.js
+++ js/js.js
@@ -1,11 +1,43 @@
-DEJavaScript
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
- 
-function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
+
+window.onscroll = function() {myFunction()};
+
+
+var nav = document.getElementById("nav");
+
+
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
